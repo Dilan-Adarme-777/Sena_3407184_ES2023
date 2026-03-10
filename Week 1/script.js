feb885813f620110d9d5aefe8579fbdf5e771eb3
LeiderDavidRinconesAlatahona <rinconestec1028@gmail.com>
2026-03-10 14:35:29 -0500

    proyectos

---
+++ Week 1/script.js
@@ -0,0 +1,48 @@
+
+console.log('=========================================');
+console.log('PLATAFORMA E-LEARNING - FICHA DE PRESENTACIÓN');
+console.log('=========================================');
+console.log('');
+
+console.log('INFORMACIÓN GENERAL');
+console.log('--------------------');
+
+console.log('Nombre:          Taller de JavaScript para Principiantes');
+
+console.log('Código:          WKS-101');
+
+console.log('Categoría:       Programación Web - Nivel Básico');
+
+console.log('Estado:          Disponible');
+
+console.log('');
+
+console.log('DETALLES');
+console.log('--------------------');
+
+console.log('Valor principal:    120');
+
+console.log('Valor calculado:    ', 120 * 0.8);
+
+console.log('Estado booleano 1:  true');
+
+console.log('Estado booleano 2:  false');
+
+console.log('');
+
+console.log('RESUMEN DEL DOMINIO');
+console.log('--------------------');
+
+console.log('Dominio:          Plataforma de talleres E-learning');
+
+console.log('Entidad:          workshop');
+
+console.log('Total registros:  45');
+
+console.log('');
+
+console.log('=========================================');
+
+console.log('  ¡Aprende nuevas habilidades con nuestros workshops online!');
+
+console.log('=========================================');
